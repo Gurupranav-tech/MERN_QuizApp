952b8fbe60d3dafdc10e273e8fcebb7fe365d195
Gurupranav-tech <67361835+Gurupranav-tech@users.noreply.github.com>
2024-10-01 15:57:57 +0530

    Connected to mongodb database using prisma

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,9 +1,18 @@
 import express from "express";
 import cookieParser from "cookie-parser";
 import httpLogger from "./middlewares/httpLogger";
+import AuthRouter from "./routes/auth";
 import { NODE_ENV, PORT } from "./config";
+import { connect as connectToDatabase } from "./db/connect";
+import Logger from "./utils/logger";
 
 // Connect to database
+try {
+  await connectToDatabase();
+} catch {
+  Logger.error("Error connecting to database");
+  process.exit(1);
+}
 
 // Create the server using express
 const app = express();
@@ -17,6 +26,7 @@ if (NODE_ENV === "development") {
 }
 
 // Add all the routes
+app.use("/auth", AuthRouter);
 
 // Start the server
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
