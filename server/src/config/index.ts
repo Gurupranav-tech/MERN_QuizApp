493ac5e26d2b704dc1252d27e9a0f2069296437f
Gurupranav-tech <67361835+Gurupranav-tech@users.noreply.github.com>
2024-10-01 20:46:56 +0530

    Added express sessions

--- server/src/config/index.ts
+++ server/src/config/index.ts
@@ -8,3 +8,5 @@ dotenvConfig({
 
 export const NODE_ENV: string = process.env.NODE_ENV || "development";
 export const PORT: number = +(process.env.PORT || "") || 5000;
+export const SESSION_SECRET =
+  process.env.SESSION_SECRET || "some super secure session key";
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,18 +1,26 @@
 import express from "express";
 import cookieParser from "cookie-parser";
+import expressSession from "express-session";
+import { PrismaSessionStore } from "@quixo3/prisma-session-store";
 import httpLogger from "./middlewares/httpLogger";
 import AuthRouter from "./routes/auth";
-import { NODE_ENV, PORT } from "./config";
-import { connect as connectToDatabase } from "./db/connect";
+import { NODE_ENV, PORT, SESSION_SECRET } from "./config";
+import prisma, { connect as connectToMongoDB } from "./db/connect";
 import Logger from "./utils/logger";
 
-// Connect to database
+// Connect to mongodb database
 try {
-  await connectToDatabase();
+  await connectToMongoDB();
 } catch {
   Logger.error("Error connecting to database");
   process.exit(1);
 }
+
+// Create a storage class for express session
+const sessionStore = new PrismaSessionStore(prisma as any, {
+  dbRecordIdIsSessionId: true,
+  dbRecordIdFunction: undefined,
+});
 
 // Create the server using express
 const app = express();
@@ -20,6 +28,18 @@ const app = express();
 // Add all the requried middlewares both in production and development mode via NODE_ENV env var
 app.use(cookieParser());
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(
+  expressSession({
+    store: sessionStore,
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+      maxAge: 1000 * 60 * 60 * 24 * 7,
+    },
+    secret: SESSION_SECRET,
+  })
+);
 
 if (NODE_ENV === "development") {
   app.use(httpLogger);
